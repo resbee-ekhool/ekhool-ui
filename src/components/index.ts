0924261e69f97a180dea9256ec5cc321be7e01ce
bavith-ekhool <bavith.resbee@gmail.com>
2023-01-05 18:20:16 +0530

    added sidebar

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,1 +1,6 @@
 export { default as Button } from "./Button";
+export { default as IconButton } from "./IconButton";
+export { default as ListItem } from "./ListItem";
+export { default as Sidebar } from "./Sidebar";
+export { default as Stack } from "./Stack";
+export { default as Typography } from "./Typography";
