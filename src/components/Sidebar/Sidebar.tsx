1170e09dfc0c4a803bae7d2a7df0f67a441403a2
bavith-ekhool <bavith.resbee@gmail.com>
2023-01-06 20:05:00 +0530

    Added header

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -7,7 +7,7 @@ import MenuItem from '../MenuItem/MenuItem';
 import Stack from '../Stack/Stack';
 import Typography from '../Typography/Typography';
 
-interface SideBarPorps {
+type SideBarPorps = {
 
     data: any,
     onChange: Function,
@@ -36,132 +36,138 @@ const SidebarArrow= () => {
     )
 }
 
-const Sidebar:FC<SideBarPorps> = ({ data, onChange = () => {}, accountUrl }) => {
+class Sidebar extends React.Component<SideBarPorps> {
 
-    const [hovered, setHovered] = useState(false);
-    const [hoveredMenu, setHoveredMenu] = useState(initialHoverMenu);
-    const [hoveredIndex, setHoveredIndex] = useState(-1);
-
-    if(!Array.isArray(data))
-    {
-        throw new Error("Error: Please check the data")
+    state = {
+        hovered: false,
+        hoveredMenu: initialHoverMenu,
+        hoveredIndex: -1
     }
 
-    const currentParentItem = data?.filter((i) => (i.active))
+    _onChangeChild = (item: any) => {
 
-    if(currentParentItem?.length < 1)
-    {
-        throw new Error("Error")
-    }
-
-    const parentItem = currentParentItem?.length > 0 && currentParentItem[0]
-    const currentItemChildren = parentItem?.children || [];
-
-    const _onChangeParent = (item: any) => {
-        
-        onChange(currentItemChildren[0]?.path)
+        this.props.onChange(item?.path)
 
     }
 
-    const _onChangeChild = (item: any) => {
+    render(): React.ReactNode {
 
-        onChange(item?.path)
+        let { hovered, hoveredMenu, hoveredIndex } = this.state;
+        const { data, accountUrl} = this.props
 
-    }
+        if(!Array.isArray(data))
+        {
+            throw new Error("Error: Please check the data")
+        }
 
-    const dataToMap = hovered ? hoveredMenu?.children : currentItemChildren;
-    const title = hovered ? hoveredMenu?.title : parentItem?.title;
+        const currentParentItem = data?.filter((i) => (i.active))
 
-    return (
-        <div 
-            onMouseLeave={(e) => {
-                if(hovered)
-                {
-                    setHovered(false)
-                    setHoveredMenu(initialHoverMenu)
-                    setHoveredIndex(-1)
-                }
-            }}
-            className='sidebar'
-        >
-            <Stack p={2} gap={2} w={1} bgColor={'bg-white'} borderEnd={1} mh={100} >
+        if(currentParentItem?.length < 1)
+        {
+            throw new Error("Error")
+        }
 
-                {
-                    data?.map((item: any, index: number) => {
+        const parentItem = currentParentItem?.length > 0 && currentParentItem[0]
+        const currentItemChildren = parentItem?.children || [];
 
-                        const { icon, title, active } = item
+        const dataToMap = hovered ? hoveredMenu?.children : currentItemChildren;
+        const title = hovered ? hoveredMenu?.title : parentItem?.title;
 
-                        return (
-                            <Stack
-                                onMouseOver={(e) => {
-                                    setHovered(true)
-                                    setHoveredMenu(item)
-                                    setHoveredIndex(index)
-                                }}
-                                p={1}
-                            >
-
-                                {
-                                    hoveredIndex === index && <SidebarArrow/>
-                                }
-                                
-                                {
-                                    typeof title === 'string' ?
-                                    <IconButton icon={icon} size='medium' titleVariant='caption' active={active} >
-                                        {title}
-                                    </IconButton>
-                                    :
-                                    <>
-                                        {title}
-                                    </>
-                                }
-
-                            </Stack>
-                        )
-                    })
-                }
-
-            </Stack>
-
-            <Stack mh={100} >
-
-                <Stack w={5} p={3} borderEnd={1} height={100} shadow={hovered ? 'side-bar-shadow' : ''} br={hovered ? 2 : 0} >
-
-                    <Typography variant={'subtitle1'} >{title}</Typography>
-
-                    <Stack gap={2} mt={2} h={75} p={1} >
-                
-                        {
-                            dataToMap?.map((item: any, index: number) => {
-
-                                const { icon, title, active, component } = item
-
-                                if(component)
-                                {
-                                    return <>{component}</>
-                                }
-
-                                return (
-                                    <ListItem active={active} title={title} icon={icon} onClick={() => { _onChangeChild(item) }} />
-                                )
-                            })
-                        }
-
-                    </Stack>
+        return (
+            <div 
+                onMouseLeave={(e) => {
+                    if(hovered)
+                    {
+                        this.setState({
+                            hovered: false,
+                            hoveredMenu: initialHoverMenu,
+                            hoveredIndex: -1
+                        })
+                    }
+                }}
+                className='sidebar'
+            >
+                <Stack p={2} gap={2} w={1} bgColor={'bg-white'} borderEnd={1} mh={100} >
 
                     {
-                        accountUrl &&
-                        <Stack h={25} w={100} >
-                            <iframe src={accountUrl} />
-                        </Stack>
+                        data?.map((item: any, index: number) => {
+
+                            const { icon, title, active } = item
+
+                            return (
+                                <Stack
+                                    onMouseOver={(e) => {
+                                        this.setState({
+                                            hovered: true,
+                                            hoveredMenu: item,
+                                            hoveredIndex: index
+                                        })
+                                    }}
+                                    p={1}
+                                >
+
+                                    {
+                                        hoveredIndex === index && <SidebarArrow/>
+                                    }
+
+                                    {
+                                        typeof title === 'string' ?
+                                        <IconButton icon={icon} size='medium' titleVariant='caption' active={active} >
+                                            {title}
+                                        </IconButton>
+                                        :
+                                        <>
+                                            {title}
+                                        </>
+                                    }
+
+                                </Stack>
+                            )
+                        })
                     }
 
                 </Stack>
 
-            </Stack>
+                <Stack mh={100} >
 
-        </div>
-    )
-};
+                    <Stack w={5} p={3} borderEnd={1} height={100} shadow={hovered ? 'side-bar-shadow' : ''} br={hovered ? 2 : 0} >
+
+                        <Typography variant={'subtitle1'} >{title}</Typography>
+
+                        <Stack gap={2} mt={2} h={75} p={1} >
+
+                            {
+                                dataToMap?.map((item: any, index: number) => {
+
+                                    const { icon, title, active, component } = item
+
+                                    if(component)
+                                    {
+                                        return <>{component}</>
+                                    }
+
+                                    return (
+                                        <ListItem active={active} title={title} icon={icon} onClick={() => { this._onChangeChild(item) }} />
+                                    )
+                                })
+                            }
+
+                        </Stack>
+
+                        {
+                            accountUrl &&
+                            <Stack h={25} w={100} >
+                                <iframe src={accountUrl} />
+                            </Stack>
+                        }
+
+                    </Stack>
+
+                </Stack>
+
+            </div>
+        )
+    }
+}
 
 export default Sidebar;
